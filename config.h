9c93a6f9abf61f0dcae8055fb404f25a766ef065
Donocean <1184427366@qq.com>
2022-09-07 16:19:38 +0800

    change font and font size

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Consolas:size=16" };
-static const char dmenufont[]       = "Consolas:size=16";
+static const char *fonts[]          = { "Sauce Code Pro NF:size=20" };
+static const char dmenufont[]       = "Sauce Code Pro NF:size=20";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
